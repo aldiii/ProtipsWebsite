24ade4056fa35ba5c0ee4de181e2f9c9b1537ad1
Aldona Tomzik <a.tomzik@gmail.com>
2020-05-26 14:51:31 +0200

    added burger menu styling and menu collapse

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,9 @@
 btnBurger = document.querySelector(".btn--burger");
+mainNav = document.getElementById("main-nav__collapse");
 
 const handleClick = function (e) {
   this.classList.toggle("active");
+  mainNav.classList.toggle("active");
 };
 
 btnBurger.addEventListener("click", handleClick);
