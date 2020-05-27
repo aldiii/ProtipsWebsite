0d9032e0389adbac960a851d743fc8c7a95cc504
Aldona Tomzik <a.tomzik@gmail.com>
2020-05-27 11:44:03 +0200

    refactored the slider

--- js/slider.js
+++ js/slider.js
@@ -1,50 +1,58 @@
-const carousel = document.querySelector(".testimonial__carousel");
-const items = document.querySelectorAll(".testimonial__slide");
-const auto = true;
-const intervalTime = 5000;
-let slideInterval;
-const slideCount = Math.ceil(
-  (carousel.offsetWidth / items[0].offsetWidth) * items.length
-);
-let currentSlide = 0;
-const dots = document.querySelector(".testimonial__dots");
-
-const runSlider = () => {
-  clearInterval(slideInterval);
-  slideInterval = setInterval(nextSlide, intervalTime);
-};
-const moveSlide = (slide) => {
-  carousel.style.transform = `translateX(${slide * -100}%)`;
-};
-const toogleActive = (dot) => {
-  dots.children[dot].classList.toggle("active");
-};
-const generateDots = function (count) {
-  for (let i = 0; i < count; ++i) {
-    const dot = document.createElement("span");
-    dot.id = i;
-    dot.classList.add("dot");
-    if (i === currentSlide) dot.classList.add("active");
-    dot.addEventListener("click", goToSlide);
-    dots.appendChild(dot);
+class Slider {
+  constructor(sectionName, auto = true, intervalTime = 5000) {
+    this.carousel = document.querySelector(`.${sectionName}__carousel`);
+    this.items = document.querySelectorAll(`.${sectionName}__slide`);
+    this.dots = document.querySelector(`.${sectionName}__dots`);
+    this.auto = auto;
+    this.intervalTime = intervalTime;
+    this.slideInterval;
+    this.slideCount = Math.ceil(
+      (this.carousel.offsetWidth / this.items[0].offsetWidth) *
+        this.items.length
+    );
+    this.currentSlide = 0;
+    this.generateDots(this.slideCount);
+    if (auto) this.runSlider();
   }
-};
 
-const goToSlide = function (e) {
-  toogleActive(currentSlide);
-  currentSlide = this.id * 1;
-  moveSlide(currentSlide);
-  toogleActive(currentSlide);
-  if (auto) runSlider;
-};
+  generateDots(count) {
+    for (let i = 0; i < count; ++i) {
+      const dot = document.createElement("span");
+      dot.id = i;
+      dot.classList.add("dot");
+      if (i === this.currentSlide) dot.classList.add("active");
+      dot.onclick = this.goToSlide.bind(this);
+      this.dots.appendChild(dot);
+    }
+  }
 
-const nextSlide = () => {
-  toogleActive(currentSlide);
-  currentSlide = currentSlide < slideCount - 1 ? ++currentSlide : 0;
-  moveSlide(currentSlide);
-  toogleActive(currentSlide);
-};
+  runSlider() {
+    this.slideInterval = setInterval(
+      this.nextSlide.bind(this),
+      this.intervalTime
+    );
+  }
+  moveSlide(slide) {
+    this.toggleActive(this.currentSlide);
+    this.currentSlide = slide;
 
-generateDots(slideCount);
+    this.carousel.style.transform = `translateX(${this.currentSlide * -100}%)`;
+    this.toggleActive(this.currentSlide);
+  }
+  toggleActive(dot) {
+    this.dots.children[dot].classList.toggle("active");
+  }
+  goToSlide(e) {
+    if (this.slideInterval) clearInterval(this.slideInterval);
+    let slide = e.target.id * 1;
+    this.moveSlide(slide);
+    if (this.auto) this.runSlider();
+  }
+  nextSlide() {
+    let slide =
+      this.currentSlide < this.slideCount - 1 ? this.currentSlide + 1 : 0;
+    this.moveSlide(slide);
+  }
+}
 
-if (auto) runSlider();
+const testimonialSlider = new Slider("testimonial");
